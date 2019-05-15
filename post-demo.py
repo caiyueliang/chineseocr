68ff4189eaafce687fa8fef0887c375a240970b6
caiyueliang <393900414@qq.com>
2019-05-15 10:13:41 +0800

    update

--- post-demo.py
+++ post-demo.py
@@ -6,28 +6,31 @@
 import base64
 import requests
 import json
-def read_img_base64(p):
-    with open(p,'rb') as f:
-        imgString = base64.b64encode(f.read())
-    imgString=b'data:image/jpeg;base64,'+imgString
-    return imgString.decode()
 
-def post(p,billModel='通用OCR'):
-    URL='http://127.0.0.1:8080/ocr'##url地址
-    imgString = read_img_base64(p)
+
+def read_img_base64(p):
+    with open(p, 'rb') as f:
+        img_string = base64.b64encode(f.read())
+    img_string = b'data:image/jpeg;base64,' + img_string
+    return img_string.decode()
+
+
+def post(p, billModel='通用OCR'):
+    URL = 'http://127.0.0.1:8080/ocr'           # url地址
+    img_string = read_img_base64(p)
     headers = {}
-    param      = {'billModel':billModel,##目前支持三种 通用OCR/ 火车票/ 身份证/
-                  'imgString':imgString,}
+    param = {'billModel': billModel,            # 目前支持三种 通用OCR/ 火车票/ 身份证/
+             'imgString': img_string}
     param = json.dumps(param)
     if 1:
-            req          =  requests.post(URL,data= param,headers=None,timeout=5)
-            data         =  req.content.decode('utf-8')
-            data         =  json.loads(data)
+        req = requests.post(URL, data=param, headers=None, timeout=5)
+        data = req.content.decode('utf-8')
+        data = json.loads(data)
     else:
-            data =[]
+        data =[]
     print(data)
 
     
 if __name__=='__main__':
     p = 'test/card.png'
-    post(p,'身份证')
+    post(p, '身份证')
